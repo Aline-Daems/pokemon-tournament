58106e82f14303b3933a798027f1071a1772e9d2
Aline Daems <daems.aline90@gmail.com>
2024-01-11 12:08:51 +0100

    add create method

--- src/main/java/be/techonbel/pokemontournament/bl/services/impl/PlayerServiceImpl.java
+++ src/main/java/be/techonbel/pokemontournament/bl/services/impl/PlayerServiceImpl.java
@@ -2,15 +2,17 @@ package be.techonbel.pokemontournament.bl.services.impl;
 
 import be.techonbel.pokemontournament.bl.services.PlayerService;
 import be.techonbel.pokemontournament.dal.models.entities.Player;
+import be.techonbel.pokemontournament.dal.repositories.PlayerRepository;
 import be.techonbel.pokemontournament.pl.forms.Playerform;
 
 public class PlayerServiceImpl implements PlayerService {
 
-    private final PlayerService playerService;
+    private final PlayerRepository playerRepository;
 
-    public PlayerServiceImpl(PlayerService playerService) {
-        this.playerService = playerService;
+    public PlayerServiceImpl(PlayerRepository playerRepository) {
+        this.playerRepository = playerRepository;
     }
+
 
     @Override
     public void create(Playerform form) {
@@ -22,6 +24,15 @@ public class PlayerServiceImpl implements PlayerService {
         Player player = new Player();
         player.setPseudo(form.pseudo());
         player.setMail(form.mail());
+        player.setPassword(form.password());
+        player.setBirthdate(form.birthdate());
+        player.setGender(form.gender());
+        player.setBadges(form.badges());
+        player.setRole(form.role());
+
+        playerRepository.save(player);
+
+
 
     }
 }
